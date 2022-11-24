eed694c6d6df7721ad310129d2057f501270a636
Dawid Łabno <dawlab@icloud.com>
2022-11-24 18:08:34 +0100

    Add Encoding and decoding data using NSCoder

---
+++ ToDoList/Controllers/ViewController.swift
@@ -0,0 +1,138 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Dawid Łabno on 16/11/2022.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
+  var itemsArray = [Task]()
+  let dataFilePath = FileManager.default.urls(
+    for: .documentDirectory,
+    in: .userDomainMask)
+    .first?.appendingPathComponent("Items.plist")
+  private lazy var textField: UITextField = {
+    let textField = UITextField()
+    textField.placeholder = "Type task title here.."
+    textField.clearsOnBeginEditing = true
+    textField.borderStyle = .roundedRect
+    textField.adjustsFontSizeToFitWidth = true
+    textField.delegate = self
+    return textField
+  }()
+  private lazy var taskList: UITableView = {
+    let taskList = UITableView()
+    taskList.delegate = self
+    taskList.dataSource = self
+    return taskList
+  }()
+  private lazy var box: UIView = {
+    let box = UIView()
+    return box
+  }()
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .white
+    loadItems()
+    layout()
+  }
+  private func layout() {
+    view.addSubview(box)
+    box.snp.makeConstraints { (make) -> Void in
+      make.left.equalTo(view)
+      make.right.equalTo(view)
+      make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+      make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+    }
+    view.addSubview(textField)
+    textField.snp.makeConstraints { (make) in
+      make.width.equalTo(380)
+      make.height.equalTo(50)
+      make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+      make.centerX.equalToSuperview()
+    }
+    view.addSubview(taskList)
+    taskList.snp.makeConstraints { (make) in
+      make.width.equalTo(380)
+      make.height.equalTo(700)
+      make.top.equalTo(textField.snp.bottom)
+    }
+  }
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return itemsArray.count
+  }
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = UITableViewCell(style: .default, reuseIdentifier: "TaskCell")
+    let task = itemsArray[indexPath.row]
+    let itemTitle = itemsArray[indexPath.row].title
+    cell.textLabel?.text = itemTitle
+    cell.accessoryType = task.done ? .checkmark : .none
+    return cell
+  }
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    itemsArray[indexPath.row].done = !itemsArray[indexPath.row].done
+    saveItems()
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
+  func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+    return .delete
+  }
+  func tableView(_ tableView: UITableView,
+                 commit editingStyle: UITableViewCell.EditingStyle,
+                 forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      tableView.beginUpdates()
+      itemsArray.remove(at: indexPath.row)
+      tableView.deleteRows(at: [indexPath], with: .fade)
+      saveItems()
+      tableView.endUpdates()
+    }
+  }
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    textField.placeholder = "Type task title here.."
+    textField.endEditing(true)
+    return true
+  }
+  func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+    if textField.text != "" {
+      return true
+    } else {
+      textField.placeholder = "Type something!"
+      return false
+    }
+  }
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    if let task = textField.text {
+      let newTask = Task()
+      newTask.title = task
+      itemsArray.append(newTask)
+      saveItems()
+    } else {
+      print("Type something")
+    }
+    textField.text = ""
+  }
+  func saveItems() {
+    let encoder = PropertyListEncoder()
+    do {
+      let data = try encoder.encode(itemsArray)
+      try data.write(to: dataFilePath!)
+    } catch {
+      print("Error encoding item array, \(error)")
+    }
+    taskList.reloadData()
+  }
+  func loadItems() {
+    if let data = try? Data(contentsOf: dataFilePath!) {
+      let decoder = PropertyListDecoder()
+      do {
+        itemsArray = try decoder.decode([Task].self, from: data)
+      } catch {
+        print("Error decoding item array, \(error)")
+      }
+    }
+  }
+}
